a37fb78a137451a89787e4f325f76e7497ad2f0a
Nicolas Ganivet <nicolas@cividesk.com>
2016-06-05 14:19:45 -0600

    Added configuration file template

---
+++ config.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * ATTENTION: You need to copy this file to 'config.php'
+ * and personalize it for your environment!
+ */
+
+$dbhost = 'mysql_host';
+$user   = 'mysql_user';
+$pass   = 'mysql_pass';
