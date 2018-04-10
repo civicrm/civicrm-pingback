9bfa37562518da13710c33a9e80def21dbf7ce2f
Tim Otten <totten@civicrm.org>
2018-04-10 00:09:10 -0700

    FullJsonReport - Cleanup; use more helpers

--- src/Report/FullJsonReport.php
+++ src/Report/FullJsonReport.php
@@ -1,6 +1,8 @@
 <?php
 namespace Pingback\Report;
 
+use Pingback\VersionsFile;
+use Pingback\VersionNumber;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,30 +16,51 @@ class FullJsonReport {
    */
   public static function generate(Request $request) {
     $versionInfo = \Pingback\VersionsFile::read(\Pingback\VersionsFile::getFileName());
+    $versionInfo = self::downgradeStatuses($versionInfo);
 
     $requestVersion = $request->get('version', '');
 
-    $output = array();
-
-    // If a version has been specified, we only return info >= to that version
-    $versionParts = explode('.', $requestVersion);
-
-    if (empty($versionParts[0]) || empty($versionParts[1]) || !is_numeric($versionParts[0]) || !is_numeric($versionParts[1])) {
+    if (empty($requestVersion) || !VersionNumber::isWellFormed($requestVersion)) {
       // No valid version specified, just return all info
       return new Response(json_encode($versionInfo), 200, [
         'Content-Type' => 'application/json',
       ]);
     }
-    $version = $versionParts[0] . '.' . $versionParts[1];
-    foreach ($versionInfo as $majorVersion => $info) {
-      if ($majorVersion >= $version) {
-        $output[$majorVersion] = $info;
+    else {
+      // If a version has been specified, we only return info >= to that version
+      $version = VersionNumber::getMinor($requestVersion);
+      $output = array();
+      foreach ($versionInfo as $branchVer => $branchDef) {
+        if ($branchVer >= $version) {
+          $output[$branchVer] = $branchDef;
+        }
+      }
+
+      return new Response(json_encode($output), 200, [
+        'Content-Type' => 'application/json',
+      ]);
+    }
+  }
+
+  /**
+   * Filter statuses that were not supported circa 4.6/4.7.
+   *
+   * @return array
+   */
+  public static function downgradeStatuses($versions) {
+    $map = array(
+      'deprecated' => 'stable',
+    );
+
+    $result = [];
+    foreach ($versions as $k => $v) {
+      $result[$k] = $v;
+      if (isset($map[$v['status']])) {
+        $v['status'] = $map[$v['status']];
       }
     }
-
-    return new Response(json_encode($output), 200, [
-      'Content-Type' => 'application/json',
-    ]);
+    return $result;
   }
+
 
 }
